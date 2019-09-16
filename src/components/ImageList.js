d8b33a63695e15c44d236bb346bac14274f565fe
alfredo <you@example.com>
2019-09-16 14:34:31 -0700

    added app.css and added alt tag to images styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../app.css';
 
 const ImageList = (props) => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} alt={image.alt_description} />
+    return <img className={`img_contain`} src={image.urls.regular} alt={image.alt_description} style={{width: '100%'}} />
   });
 
-  return <div>{images}</div>;
+  return <div className="images_container">{images}</div>;
 };
 
 export default ImageList;
