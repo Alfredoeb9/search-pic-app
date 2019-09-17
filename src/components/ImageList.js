84428597127954d3b0b409fd05e6d45f2cc6448d
alfredo <you@example.com>
2019-09-16 18:26:20 -0700

    images display with grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import '../app.css';
 
 const ImageList = (props) => {
   const images = props.images.map(image => {
-    return <img className={`img_contain`} src={image.urls.regular} alt={image.alt_description} style={{width: '100%'}} />
+    return <img className={`img_contain`} key={image.id} src={image.urls.regular} alt={image.description} style={{width: '100%'}} />
   });
 
   return <div className="images_container">{images}</div>;
